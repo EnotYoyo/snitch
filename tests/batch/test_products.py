45a75e4d43aeb0b21acdb17117e62bbd126fa6b2
Enot <enot@scryptmail.com>
2018-04-27 01:52:38 +0300

    fix test and change ' to "

--- tests/batch/test_products.py
+++ tests/batch/test_products.py
@@ -8,9 +8,9 @@ from tests.utils import empty_database, send_json, app
 
 
 def test_product_empty_list(empty_database, app):
-    send = functools.partial(send_json, app, 'get', '/products')
+    send = functools.partial(send_json, app, "get", "/products")
 
-    response = app.get('/products')
+    response = app.get("/products")
     assert response.get_json() == {"products": []}
 
     response = send(dict(count=100))
@@ -24,33 +24,33 @@ def test_product_empty_list(empty_database, app):
 
 
 def test_product_create(empty_database, app):
-    send = functools.partial(send_json, app, 'post', '/products')
+    send = functools.partial(send_json, app, "post", "/products")
 
-    response = app.post('/products')
-    assert response.get_json() == {'message': {'name': 'Missing required parameter in the JSON body'}}
+    response = app.post("/products")
+    assert response.get_json() == {"message": {"name": "Missing required parameter in the JSON body"}}
 
     response = send(dict(name="Product 1"))
-    assert response.get_json() == {'message': {'description': 'Missing required parameter in the JSON body'}}
+    assert response.get_json() == {"message": {"description": "Missing required parameter in the JSON body"}}
 
     response = send(dict(name="Product 1", description="My test product"))
-    assert response.get_json() == [{'id': 1, 'name': "Product 1", 'description': "My test product"}, 201]
+    assert response.get_json() == [{"id": 1, "name": "Product 1", "description": "My test product"}, 201]
 
 
-def create_not_unique(empty_database, app):
-    send = functools.partial(send_json, app, 'post', '/products')
+def test_create_not_unique(empty_database, app):
+    send = functools.partial(send_json, app, "post", "/products")
     response = send(dict(name="Product 1", description="My test product"))
-    assert response.get_json() == [{'id': 1, 'name': "Product 1", 'description': "My test product"}, 201]
+    assert response.get_json() == [{"id": 1, "name": "Product 1", "description": "My test product"}, 201]
 
     response = send(dict(name="Product 1", description="My test product [2]"))
-    assert response.get_json() == 409
+    assert response.status_code == 409
 
 
 def test_product_list(empty_database, app):
-    send = functools.partial(send_json, app, 'post', '/products')
+    send = functools.partial(send_json, app, "post", "/products")
     products = [
-        {'id': 1, 'name': "Product 0", 'description': "My test product [0]"},
-        {'id': 2, 'name': "Product 1", 'description': "My test product [1]"},
-        {'id': 3, 'name': "Product 2", 'description': "My test product [2]"}
+        {"id": 1, "name": "Product 0", "description": "My test product [0]"},
+        {"id": 2, "name": "Product 1", "description": "My test product [1]"},
+        {"id": 3, "name": "Product 2", "description": "My test product [2]"}
     ]
     response = send(dict(name="Product 0", description="My test product [0]"))
     assert response.get_json() == [products[0], 201]
@@ -61,9 +61,9 @@ def test_product_list(empty_database, app):
     response = send(dict(name="Product 2", description="My test product [2]"))
     assert response.get_json() == [products[2], 201]
 
-    send_get = functools.partial(send_json, app, 'get', '/products')
+    send_get = functools.partial(send_json, app, "get", "/products")
 
-    response = app.get('/products')
+    response = app.get("/products")
     assert response.get_json() == {"products": products}
 
     response = send_get(dict(count=1))
--- tests/batch/test_reviews.py
+++ tests/batch/test_reviews.py
@@ -22,9 +22,9 @@ def empty_database():
 
 
 def test_review_empty_list(empty_database, app):
-    send = functools.partial(send_json, app, 'get', '/reviews')
+    send = functools.partial(send_json, app, "get", "/reviews")
 
-    response = app.get('/reviews')
+    response = app.get("/reviews")
     assert response.get_json() == {"reviews": []}
 
     response = send(dict(count=100))
@@ -38,32 +38,32 @@ def test_review_empty_list(empty_database, app):
 
 
 def test_review_create(empty_database, app):
-    send = functools.partial(send_json, app, 'post', '/reviews')
+    send = functools.partial(send_json, app, "post", "/reviews")
 
-    response = app.post('/reviews')
-    assert response.get_json() == {'message': {'product_id': 'Missing required parameter in the JSON body'}}
+    response = app.post("/reviews")
+    assert response.get_json() == {"message": {"product_id": "Missing required parameter in the JSON body"}}
 
     response = send(dict(product_id=1))
-    assert response.get_json() == {'message': {'review': 'Missing required parameter in the JSON body'}}
+    assert response.get_json() == {"message": {"review": "Missing required parameter in the JSON body"}}
 
     d = datetime.datetime.utcnow()
-    with mock.patch('datetime.datetime') as patched:
+    with mock.patch("datetime.datetime") as patched:
         patched.utcnow = mock.Mock(return_value=d)
         response = send(dict(product_id=1, review="My test review"))
-        assert response.get_json() == [{'id': 1, 'review': "My test review", "created_time": int(d.timestamp())}, 201]
+        assert response.get_json() == [{"id": 1, "review": "My test review", "created_time": int(d.timestamp())}, 201]
 
 
 def test_product_list(empty_database, app):
     d = datetime.datetime.utcnow()
-    with mock.patch('datetime.datetime') as patched:
+    with mock.patch("datetime.datetime") as patched:
         patched.utcnow = mock.Mock(return_value=d)
         timestamp = int(d.timestamp())
 
-        send = functools.partial(send_json, app, 'post', '/reviews')
+        send = functools.partial(send_json, app, "post", "/reviews")
         review = [
-            {'id': 1, 'review': "My test review [0]", "created_time": timestamp},
-            {'id': 2, 'review': "My test review [1]", "created_time": timestamp},
-            {'id': 3, 'review': "My test review [2]", "created_time": timestamp}
+            {"id": 1, "review": "My test review [0]", "created_time": timestamp},
+            {"id": 2, "review": "My test review [1]", "created_time": timestamp},
+            {"id": 3, "review": "My test review [2]", "created_time": timestamp}
         ]
 
         response = send(dict(product_id=1, review=review[0]["review"]))
@@ -75,9 +75,9 @@ def test_product_list(empty_database, app):
         response = send(dict(product_id=2, review=review[2]["review"]))
         assert response.get_json() == [review[2], 201]
 
-        send_get = functools.partial(send_json, app, 'get', '/reviews')
+        send_get = functools.partial(send_json, app, "get", "/reviews")
 
-        response = app.get('/reviews')
+        response = app.get("/reviews")
         assert response.get_json() == {"reviews": review}
 
         response = send_get(dict(count=1))
