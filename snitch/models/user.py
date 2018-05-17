d7f015d827b4b90b1aea12f2f13cdaac2def7603
Enot <enot@scryptmail.com>
2018-05-17 22:02:37 +0300

    append vk_id column for user

--- snitch/models/user.py
+++ snitch/models/user.py
@@ -3,6 +3,7 @@ from snitch import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    vk_id = db.Column(db.Integer, nullable=False, unique=True, index=True)
     login = db.Column(db.String(64), nullable=False, unique=True)
     hash = db.Column(db.String(64), nullable=False)
 
--- tests/models/test_model_user.py
+++ tests/models/test_model_user.py
@@ -6,54 +6,71 @@ from tests.utils import empty_database
 
 
 def test_connect(empty_database):
-    product = User.query.all()
-    assert len(product) == 0
+    users = User.query.all()
+    assert len(users) == 0
 
 
 def test_create(empty_database):
-    user = User(login="login test_create", hash="hash test_create")
+    user = User(login="login test_create", vk_id=43, hash="hash test_create")
     empty_database.session.add(user)
     empty_database.session.commit()
     assert user.id == 1
+    assert user.vk_id == 43
     assert user.login == "login test_create"
     assert user.hash == "hash test_create"
 
 
 def test_russian_create(empty_database):
-    user = User(login="логин from russia", hash="hash test_create")
+    user = User(login="логин from russia", vk_id=43, hash="hash test_create")
     empty_database.session.add(user)
     empty_database.session.commit()
     assert user.id == 1
+    assert user.vk_id == 43
     assert user.login == "логин from russia"
     assert user.hash == "hash test_create"
 
 
 def test_unique_login(empty_database):
-    user1 = User(login="login test_unique_login", hash="hash1")
-    user2 = User(login="login test_unique_login", hash="hash2")
+    user1 = User(login="login test_unique_login", vk_id=23, hash="hash1")
+    user2 = User(login="login test_unique_login", vk_id=43, hash="hash2")
+    empty_database.session.add_all([user1, user2])
+    with pytest.raises(sqlalchemy.exc.IntegrityError):
+        empty_database.session.commit()
+
+
+def test_unique_vk_id(empty_database):
+    user1 = User(login="test_unique_vk_id1", vk_id=43, hash="hash1")
+    user2 = User(login="test_unique_vk_id2", vk_id=43, hash="hash2")
     empty_database.session.add_all([user1, user2])
     with pytest.raises(sqlalchemy.exc.IntegrityError):
         empty_database.session.commit()
 
 
 def test_nullable_login(empty_database):
-    user = User(hash="hash")
+    user = User(vk_id=43, hash="hash")
     empty_database.session.add(user)
     with pytest.raises(sqlalchemy.exc.IntegrityError):
         empty_database.session.commit()
 
 
 def test_nullable_hash(empty_database):
-    user = User(login="login test_nullable_hash")
+    user = User(login="login test_nullable_hash", vk_id=43)
+    empty_database.session.add(user)
+    with pytest.raises(sqlalchemy.exc.IntegrityError):
+        empty_database.session.commit()
+
+
+def test_nullable_vk_id(empty_database):
+    user = User(login="login test_nullable_hash", hash="hash")
     empty_database.session.add(user)
     with pytest.raises(sqlalchemy.exc.IntegrityError):
         empty_database.session.commit()
 
 
 def test_autoincrement(empty_database):
-    user1 = User(login="login test_autoincrement1", hash="hash1")
-    user2 = User(login="login test_autoincrement2", hash="hash2")
-    user3 = User(login="login test_autoincrement3", hash="hash2")
+    user1 = User(login="login test_autoincrement1", vk_id=4, hash="hash1")
+    user2 = User(login="login test_autoincrement2", vk_id=8, hash="hash2")
+    user3 = User(login="login test_autoincrement3", vk_id=15, hash="hash2")
     empty_database.session.add_all([user1, user2, user3])
     empty_database.session.commit()
     assert user1.id == 1
